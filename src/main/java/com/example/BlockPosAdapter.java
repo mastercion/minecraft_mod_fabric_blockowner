64e6d0ae0e376528531acffa6f053a1f3724f155
Andrej Wilhelm <andrejw39v2@gmail.com>
2024-07-22 16:52:23 +0200

    PlacedBlock: Fix User JSON loading This fixes but changes how the User json gets stored. It is incompatible with every version 1.0.2 or below. Later will be an automatic transition tool provided.

--- src/main/java/com/example/BlockPosAdapter.java
+++ src/main/java/com/example/BlockPosAdapter.java
@@ -1,27 +1,26 @@
 package com.example;
 
 import com.google.gson.*;
-
 import net.minecraft.util.math.BlockPos;
 
 import java.lang.reflect.Type;
 
 public class BlockPosAdapter implements JsonSerializer<BlockPos>, JsonDeserializer<BlockPos> {
+
     @Override
     public JsonElement serialize(BlockPos src, Type typeOfSrc, JsonSerializationContext context) {
-        JsonObject obj = new JsonObject();
-        obj.addProperty("x", src.getX());
-        obj.addProperty("y", src.getY());
-        obj.addProperty("z", src.getZ());
-        return obj;
+        return new JsonPrimitive(src.getX() + "," + src.getY() + "," + src.getZ());
     }
 
     @Override
     public BlockPos deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
-        JsonObject obj = json.getAsJsonObject();
-        int x = obj.get("x").getAsInt();
-        int y = obj.get("y").getAsInt();
-        int z = obj.get("z").getAsInt();
+        String[] parts = json.getAsString().split(",");
+        if (parts.length != 3) {
+            throw new JsonParseException("Invalid BlockPos string: " + json.getAsString());
+        }
+        int x = Integer.parseInt(parts[0]);
+        int y = Integer.parseInt(parts[1]);
+        int z = Integer.parseInt(parts[2]);
         return new BlockPos(x, y, z);
     }
 }
